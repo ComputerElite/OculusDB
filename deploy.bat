98f72df43a0778e24960982554a43965e131c4d5
ComputerElite <>
2022-04-14 10:13:00 +0200

    Update deploy script

--- deploy.bat
+++ deploy.bat
@@ -11,11 +11,12 @@ git push origin main
 echo Pushed to GitHub
 
 echo Commit id %id%
+
+cd bin\Debug\net6.0\
 echo Deleting existing update zip
-del net6.0.zip"
+del net6.0.zip
 
 echo Creating new update zip
-cd bin\Debug\net6.0\
 7z a net6.0.zip *.dll *.pdb *.exe *.json frontend ref runtimes
 
 echo Created update zip
@@ -24,3 +25,6 @@ echo.
 echo %changelog%\nFull changes: https://github.com/ComputerElite/OculusDB/commit/18fc696eeb74f8e7a4df03b4596501f02d2f2989
 
 start "" https://oculusdb.rui2015.me/admin
+cd..
+cd..
+cd..
