4b39fa2889cf0a835578761bf342eeade496e242
ComputerElite <>
2022-04-26 14:47:11 +0200

    Fix scraping

--- OculusDBServer.cs
+++ OculusDBServer.cs
@@ -93,7 +93,7 @@ namespace OculusDB
             /////////////////////////////////////////////
             // DON'T FORGET TO ADD IT BACK EVERY TIME. //
             /////////////////////////////////////////////
-            //OculusScraper.StartScrapingThread();
+            OculusScraper.StartScrapingThread();
 
             //DiscordWebhookSender.SendActivity(DateTime.Now - new TimeSpan(7, 0, 0));
 
