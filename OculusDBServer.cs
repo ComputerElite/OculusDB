106dba69e346e8009d0fb4e68e6b01a4a5be2d7e
ComputerElite <ComputerElite@CrashGods>
2022-07-02 14:30:38 +0200

    Add back caching

--- OculusDBServer.cs
+++ OculusDBServer.cs
@@ -102,7 +102,7 @@ namespace OculusDB
             FileManager.CreateDirectoryIfNotExisting(OculusDBEnvironment.dataDir + "images");
 
             // Comment if not in dev env
-            server.DefaultCacheValidityInSeconds = 0;
+            //server.DefaultCacheValidityInSeconds = 0;
 
             OculusInteractor.Init();
             MongoDBInteractor.Initialize();
