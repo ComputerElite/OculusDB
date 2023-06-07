01a14c8c792c883334580f10e3c22fda5787dc36
ComputerElite <ComputerElite@CrashGods>
2023-06-07 10:18:23 +0200

    Update deploy script, push changes

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,6 @@
 @echo off
 echo replacing frontend dir with the one from Release
-xcopy /Y /E /H /C /I "bin\Release\net6.0\frontend" frontend
-xcopy /Y /E /H /C /I "bin\Release\net6.0\frontend" "bin\Debug\net6.0\frontend"
+xcopy /Y /E /H /C /I frontend "bin\Release\net6.0\frontend"
 
 set /p changelog=Changelog: 
 echo Pushing to GitHub
@@ -14,20 +13,6 @@ git push origin main
 echo Pushed to GitHub
 
 echo Commit id %id%
-
-cd bin\Debug\net6.0\
-echo Deleting existing update zip
-del net6.0.zip
-echo %id% > commit.txt
-
-echo Creating new update zip
-7z a net6.0.zip *.dll *.pdb *.exe *.json commit.txt frontend ref runtimes
-
-echo Created update zip
-echo Changelog:
-echo.
-echo %changelog%\nFull changes: https://github.com/ComputerElite/OculusDB/commit/%id%
-
 start "" https://manage.rui2015.me/
 cd..
 cd..
