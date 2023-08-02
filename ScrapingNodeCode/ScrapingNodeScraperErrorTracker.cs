497e74fbd9140d3963270729a0366c348fb014f7
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2023-08-02 13:54:44 +0200

    Update error count needed for rate limit to 1ß

--- ScrapingNodeCode/ScrapingNodeScraperErrorTracker.cs
+++ ScrapingNodeCode/ScrapingNodeScraperErrorTracker.cs
@@ -15,7 +15,7 @@ public class ScrapingNodeScraperErrorTracker
     {
         errorTimes.Add(DateTime.UtcNow);
         errorTimes.RemoveAll(x => x < DateTime.UtcNow.AddMinutes(-5));
-        // If more than 25 errors occur in 5 minutes, the node will stop scraping for 5 hours
-        if(errorTimes.Count > 25) continueTime = DateTime.UtcNow.AddHours(5);
+        // If more than 10 errors occur in 5 minutes, the node will stop scraping for 5 hours
+        if(errorTimes.Count > 10) continueTime = DateTime.UtcNow.AddHours(5);
     }
 }
