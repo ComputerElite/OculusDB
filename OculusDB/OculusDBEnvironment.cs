d40abf3938c9f4cfa2751c964590fda744e64cdb
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2023-11-23 08:16:46 +0100

    Update OGQLAL

--- OculusDB/OculusDBEnvironment.cs
+++ OculusDB/OculusDBEnvironment.cs
@@ -13,7 +13,7 @@ namespace OculusDB
 {
     public class OculusDBEnvironment
     {
-        public static Updater updater = new ("1.1.39", "https://github.com/ComputerElite/OculusDB", "OculusDB", "OculusDB.dll");
+        public static Updater updater = new ("1.1.40", "https://github.com/ComputerElite/OculusDB", "OculusDB", "OculusDB.dll");
         public static string workingDir = "";
         public static string dataDir = "";
         // Set to false if not in dev mode
