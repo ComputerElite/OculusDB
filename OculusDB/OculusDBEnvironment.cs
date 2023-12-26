a2347d2fb7026380a6b8c6b4b03e1fab245a3ccb
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2023-12-26 23:35:37 +0100

    bump bump bmup

--- OculusDB/OculusDBEnvironment.cs
+++ OculusDB/OculusDBEnvironment.cs
@@ -13,7 +13,7 @@ namespace OculusDB
 {
     public class OculusDBEnvironment
     {
-        public static Updater updater = new ("2.2.3", "https://github.com/ComputerElite/OculusDB", "OculusDB", "OculusDB.dll");
+        public static Updater updater = new ("2.2.4", "https://github.com/ComputerElite/OculusDB", "OculusDB", "OculusDB.dll");
         public static string workingDir = "";
         public static string dataDir = "";
         // Set to false if not in dev mode
