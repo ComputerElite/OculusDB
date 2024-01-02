7e6b91df8ce42011bba189f706eb44b6897e52b2
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2024-01-02 11:12:57 +0100

    Fix recursion I hope

--- OculusDB/OculusDBEnvironment.cs
+++ OculusDB/OculusDBEnvironment.cs
@@ -10,10 +10,10 @@ using ComputerUtils.Updating;
 using OculusDB.ScrapingNodeCode;
 
 namespace OculusDB
-{
+{   
     public class OculusDBEnvironment
     {
-        public static Updater updater = new ("2.2.17", "https://github.com/ComputerElite/OculusDB", "OculusDB", "OculusDB.dll");
+        public static Updater updater = new ("2.2.18", "https://github.com/ComputerElite/OculusDB", "OculusDB", "OculusDB.dll");
         public static string workingDir = "";
         public static string dataDir = "";
         // Set to false if not in dev mode
