659d000be8ccf50dcc7834a320bd6f2fe35b5dc3
phaze-the-dumb <phaze@duck.com>
2024-02-21 18:51:47 +0000

    ring ring ring ring ring ring ring banana phone

--- OculusDB/frontend/src/index.tsx
+++ OculusDB/frontend/src/index.tsx
@@ -1,6 +1,5 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
-import Lenis from '@studio-freight/lenis'
 
 import './index.css'
 import App from './Components/App/App'
@@ -8,15 +7,3 @@ import App from './Components/App/App'
 const root = document.getElementById('root')
 
 render(() => <div><App /></div>, root!)
-
-const lenis = new Lenis({
-  wrapper: root!,
-  content: root!.firstChild! as HTMLElement
-});
-
-let raf = ( time: number ) => {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
