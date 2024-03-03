09ae2dd49154b8efcaef417356b635ee61c41bec
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2024-03-03 17:55:48 +0100

    Update Footer.tsx

--- OculusDB/frontend/src/Components/Footer/Footer.tsx
+++ OculusDB/frontend/src/Components/Footer/Footer.tsx
@@ -53,7 +53,8 @@ let Footer = () => {
     "\"also computer is in bed (edited) with me\" - Face",
     "\"I'm new around these streets\" - Sander",
     "\"6€ is a lot for some people\" - Phaze",
-    "\"Wait you're not topping me?\" - Computer to Phaze"
+    "\"Wait you're not topping me?\" - Computer to Phaze",
+    "\"omg, i have one of those made of ethernet cables\" - Phaze"
   ]
 
   let randomQuote = () => {
